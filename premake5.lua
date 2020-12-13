25c21fc5bbf3e3ba14c5460e8b2ae28996064deb
Ravi Mohan <ravimohan1991@gmail.com>
2020-12-13 18:26:03 +0530

    Added premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,43 @@
+-- premake5.lua
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("build/" .. outputdir .. "/%{prj.name}")
+	objdir ("obj/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp",
+		"imgui_tables.cpp"
+	}
+
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "off"
+
+	filter "system:linux"
+		-- https://github.com/premake/premake-core/wiki/pic
+		pic "On"
+		buildoptions "-std=c++11"
+		systemversion "latest"
+		staticruntime "off"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
